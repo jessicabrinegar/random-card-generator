360bc433e1fecd3e775e67cd904348401f964f69
jessicabrinegar <jessicabrinegar@outlook.com>
2023-01-13 20:48:38 +0000

    first version--want to make it more complex

--- src/app.js
+++ src/app.js
@@ -1,11 +1,61 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
+
+const cardNumbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const cardSuits = [
+  "bi-suit-diamond-fill",
+  "bi-suit-club-fill",
+  "bi-suit-spade-fill",
+  "bi-suit-heart-fill"
+];
+let button = document.querySelector(".btn");
+
+function randomChoice(array) {
+  let index = Math.floor(Math.random() * array.length);
+  let choice = array[index];
+  return choice;
+}
+let cardNumber;
+let cardSuit;
+function newCard() {
+  let cardNums = document.querySelectorAll("h1");
+  let suits = document.querySelectorAll(".bi");
+  cardSuit = randomChoice(cardSuits);
+  cardNumber = randomChoice(cardNumbers);
+  for (let suit of suits) {
+    let classList = suit.classList;
+    suit.classList.replace(classList[1], cardSuit);
+  }
+  for (let num of cardNums) {
+    num.innerHTML = cardNumber;
+  }
+}
+button.addEventListener("click", newCard);
+let amount;
+function addCenterIcons() {
+  if (cardNumber >= 2 || cardNumber <= 10) {
+    amount = parseInt(cardNumber);
+  } else return;
+  let center = document.querySelector(".card-body");
+  for (let i = 0; i < amount; i++) {
+    center.innerHTML += `<i class="bi ${cardSuit} fa-6x"></i>`;
+  }
+}
